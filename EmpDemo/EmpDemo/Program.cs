01ec9cb5c7ac2d89d31d12964df09e4b48a60ee4
Krish Modiya <148217072+BlackCaps24@users.noreply.github.com>
2024-08-03 19:16:20 +0530

    Add files via upload

---
+++ EmpDemo/EmpDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            
+            Console.WriteLine("Enter Employee Name :");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Enter Salary :");
+            Int32 salary = Convert.ToInt32(Console.ReadLine());
+
+            emp.SetName(name);
+            emp.Salary = salary;
+
+            emp.CalculateBonus();
+            emp.CalaculateNetSalary();
+
+            Tuple<String, int, float, float> result = emp.GetData();
+            Console.WriteLine("Enter Employee Name :"+result.Item1);
+            Console.WriteLine("Enter Salary :" + result.Item2);
+            Console.WriteLine(" Bonus :" + result.Item3);
+            Console.WriteLine("Net Salary :" + result.Item4);
+            Console.ReadKey();
+        }
+    }
+}
